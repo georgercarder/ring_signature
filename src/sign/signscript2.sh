69a50a521df565655270950d3316674200d8427c
George Carder <georgercarder@gmail.com>
2017-12-10 14:01:10 -0800

    progres on signscript2.sh. still need to scramble sets and write cases for r

--- src/sign/signscript2.sh
+++ src/sign/signscript2.sh
@@ -5,25 +5,72 @@
 
 # second part of signscript...
 
-r=$(( RANDOM % $(( $c + 1 )) ) + 1 ))
+r=$(( RANDOM % $(( $c + 1 )) + 1 ))
+
+
+echo "need to rearrange iy iP etc"
+
+hereeeeeeeeeeeeee #deliberate error to remind to rearrange
 
 # cases to get my and mx
 
 #write function here for E_k(A+B)
 
 
-case "$r" in
+function Ek(){
+	openssl enc -in $1 -out E -e -aes256 -k sign/k/keyhash.txt
+}
 
-$(( $c + 1  )) )	A=1y
-			B=v
-			xor
+function Ekinv(){
+	openssl enc -in $1 -out Einv -e -aes256 -k sign/k/keyhash.txt
+}
 
-1)
+function xor(){
+	A=$1
+	B=$2
+	./sign/xorhexdumpstobinary.sh
+}
 
-*)
+function S(){
+	case "$1" in
+
+	1) E $(xor $(cat sign/outputs.local/1y) $(cat sign/value.random/v) )
+
+	*) E $(xor $1\y $(S $(( $i - 1 )) ) )	
+	
+	esac
+}
+
+function Sinv(){
+
+	case "$1" in
+
+	$(( $c + 1 ))) Ekinv $( xor $(cat sign/outputs.local/$(( $c + 1 ))\y) $( Ekinv $sign/value.random/v))
+
+	*)
 
 
-esac
+
+}
+
+
+
+
+
+
+
+#case "$r" in
+
+#$(( $c + 1  )) )	A=1y
+#			B=v
+#			xor
+#
+#1)
+
+#*)
+
+
+#esac
 
 
 
