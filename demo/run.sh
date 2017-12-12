51114d588afb7dd03cf2a7f990efda1936baa59d
George Carder <georgercarder@gmail.com>
2017-12-11 20:17:06 -0800

    demo restore

--- demo/run.sh
+++ demo/run.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 
-c=$(($(ls publickeys.local -l|wc -l)-2))
+c=$(($(ls publickeys.local -l|wc -l)-1))
 
 
 cp -r message sign
 cp -r mykeys sign
 cp -r publickeys.local sign
 
-rm -r sign/publickeys.local/privategenerators
+#rm -r sign/publickeys.local/privategenerators
 
 
 c=$(($(ls -l|wc -l)-1))
+echo $c c is
 export c
 
 ./sign/signscript.sh
-#./sign/signscript2.sh
+./sign/signscript2.sh
 
+./sign/signscript3.sh
+
+#./validate.signed/validatesigned.sh
+
+#mv validate signed
 
 
 ####
